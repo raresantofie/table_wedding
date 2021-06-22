92f3672a956c7d17693c9de5ad02d7ac92e77185
Rares_Antofie <RA089596@cerner.net>
2021-06-22 15:21:20 +0300

    remove create

--- src/main/java/com/example/mese_nunta/TableController.java
+++ src/main/java/com/example/mese_nunta/TableController.java
@@ -3,7 +3,9 @@ package com.example.mese_nunta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TableController {
@@ -25,6 +27,17 @@ public class TableController {
     @PostMapping()
     public Table create(@RequestBody Table t) {
         return tableRepository.save(t);
+    }
+
+    @PutMapping()
+    public Table update(@RequestBody Long id) {
+        Optional<Table> table = tableRepository.findById(id);
+        if (table.isPresent()) {
+            Table t = table.get();
+            t.setChecked(true);
+            tableRepository.save(t);
+        }
+        return tableRepository.findById(id).get();
     }
 
 }
